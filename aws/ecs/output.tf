e3c2312ccb21df838d211d11a1312aa5f0e2a39e
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-11-09 12:39:29 -0600

    Create output.tf

---
+++ aws/ecs/output.tf
@@ -0,0 +1,19 @@
+output "alb" {
+  value = aws_security_group.alb.id
+}
+
+output "service" {
+  value = aws_security_group.service.id 
+}
+
+output "subnet_public_1" {
+  value = aws_security_group.service.id 
+}
+
+output "subnet_public_1" {
+  value = aws_security_group.service.id 
+}
+
+output "subnet_public_1" {
+  value = aws_security_group.service.id 
+}
