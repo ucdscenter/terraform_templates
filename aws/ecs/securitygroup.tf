39516daa3098d8fc4b5fbe11d0b757bf09effe4c
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-12-07 18:07:25 -0600

    Update securitygroup.tf

--- aws/ecs/securitygroup.tf
+++ aws/ecs/securitygroup.tf
@@ -1,77 +1,101 @@
 #ALB security group 
-resource "aws_security_group"  "alb" {
+resource "aws_security_group" "alb" {
   name   = "${var.name}-alb"
-  vpc_id = var.vpc_id  
- 
-  ingress {
-   protocol         = "tcp"
-   from_port        = 80
-   to_port          = 80
-   cidr_blocks      = ["0.0.0.0/0"]
+  vpc_id = aws_vpc.dsc_vpc.id
 
-  }
- 
   ingress {
-   protocol         = "tcp"
-   from_port        = 443
-   to_port          = 443
-   cidr_blocks      = ["0.0.0.0/0"]
-  
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   ingress {
-   protocol         = "tcp"
-   from_port        = 3000
-   to_port          = 3000
-   cidr_blocks      = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
 
   }
- 
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8001
+    to_port     = 8001
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 6379
+    to_port     = 6379
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8000
+    to_port     = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
   egress {
-   protocol         = "-1"
-   from_port        = 0
-   to_port          = 0
-   cidr_blocks      = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_security_group" "service" {
   name   = "${var.name}-service"
-  vpc_id = var.vpc_id
- 
+  vpc_id = aws_vpc.dsc_vpc.id
+
   ingress {
-   protocol         = "tcp"
-   from_port        = 80
-   to_port          = 80
-   cidr_blocks      = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-   ingress {
-   protocol         = "tcp"
-   from_port        = 443
-   to_port          = 443
-   cidr_blocks      = ["0.0.0.0/0"]
-  }
- 
-   ingress {
-   protocol         = "tcp"
-   from_port        = 8001
-   to_port          = 8001
-   cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
- ingress {
-   protocol         = "tcp"
-   from_port        = 8000
-   to_port          = 8000
-   cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8001
+    to_port     = 8001
+    cidr_blocks = ["0.0.0.0/0"]
   }
- 
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 6379
+    to_port     = 6379
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8000
+    to_port     = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-   protocol         = "-1"
-   from_port        = 0
-   to_port          = 0
-   cidr_blocks      = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
