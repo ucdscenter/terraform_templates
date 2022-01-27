4ba70fccac78fafa86cfd17736f91fd6f4942aaa
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2022-01-27 11:58:10 -0600

    Update ecs.tf

--- aws/ecs/ecs.tf
+++ aws/ecs/ecs.tf
@@ -11,49 +11,49 @@ resource "aws_cloudwatch_log_stream" "dsc_lma_stream" {
 }
 
 #ALB 
-#resource "aws_lb" "mla_lb" {
-#  name               = "${var.name}-lb"
-#  internal           = false
-#  load_balancer_type = "application"
-#  security_groups    = [aws_security_group.alb.id]
-#  subnets            = [aws_subnet.dsc_public_subnets[0].id, aws_subnet.dsc_public_subnets[1].id]
-#
-#  enable_deletion_protection = false
-#}
+resource "aws_lb" "mla_lb" {
+  name               = "${var.name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = [aws_subnet.dsc_public_subnets[0].id, aws_subnet.dsc_public_subnets[1].id]
+
+  enable_deletion_protection = false
+}
 
 #ALB target group
-#resource "aws_alb_target_group" "mla_alb_tg_group" {
-#  name = "${var.name}-tg"
-#  port = 80
-#
-#  protocol    = "HTTP"
-#  vpc_id      = aws_vpc.dsc_vpc.id
-#  target_type = "ip"
-#
-# health_check {
-#  healthy_threshold   = "10"
-#  interval            = "30"
-#  protocol            = "HTTP"
-#  timeout             = "20"
-#  path                = var.health_check_path
-#  unhealthy_threshold = "2"
-#}
-#
-#  depends_on = [aws_lb.mla_lb]
-#}
+resource "aws_alb_target_group" "mla_alb_tg_group" {
+  name = "${var.name}-tg"
+  port = 80
 
-#resource "aws_alb_listener" "ecs_alb_http_listner" {
-#  load_balancer_arn = aws_lb.mla_lb.id
-#  port              = 80
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_alb_target_group.mla_alb_tg_group.arn
-#  }
-#
-#  depends_on = [aws_alb_target_group.mla_alb_tg_group]
-#}
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.dsc_vpc.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "10"
+    interval            = "30"
+    protocol            = "HTTP"
+    timeout             = "20"
+    path                = var.health_check_path
+    unhealthy_threshold = "2"
+}
+
+  depends_on = [aws_lb.mla_lb]
+}
+
+resource "aws_alb_listener" "ecs_alb_http_listner" {
+  load_balancer_arn = aws_lb.mla_lb.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.mla_alb_tg_group.arn
+  }
+
+  depends_on = [aws_alb_target_group.mla_alb_tg_group]
+}
 
 #Redirect traffic to target group
 #resource "aws_alb_listener" "ecs_alb_https_listner" {
@@ -288,9 +288,9 @@ resource "aws_ecs_service" "uwsgi" {
     assign_public_ip = true
   }
 
-  #load_balancer {
-  #  target_group_arn = aws_alb_target_group.mla_alb_tg_group.arn
-  #  container_name   = "nginx"
-  #  container_port   = "80"            
-  #}
+  load_balancer {
+    target_group_arn = aws_alb_target_group.mla_alb_tg_group.arn
+    container_name   = "nginx"
+    container_port   = "80"            
+  }
 }
